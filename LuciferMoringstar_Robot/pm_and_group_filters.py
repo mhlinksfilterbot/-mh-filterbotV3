84f97a30c27b4128acf246ab3dfb7d32f6ce588c
mhlinksfilterbot <107455088+mhlinksfilterbot@users.noreply.github.com>
2022-06-16 17:35:29 +0530

    Update pm_and_group_filters.py

--- LuciferMoringstar_Robot/pm_and_group_filters.py
+++ LuciferMoringstar_Robot/pm_and_group_filters.py
@@ -7,7 +7,26 @@ from LuciferMoringstar_Robot.database.users_chats_db import db
 @LuciferMoringstar_Robot.on_message(Worker.text & Worker.group & Worker.incoming & Worker.chat(AUTH_GROUPS) if AUTH_GROUPS else Worker.text & Worker.group & Worker.incoming)
 async def groupfilters(client, message):
     await group_filters(client, message)
-
+else:
+        cap = f"<b><i>Movie Name : {search}\nRequested By : {message.from_user.mention}\nGroup : {message.chat.title}</i></b>"
+    if imdb and imdb.get('poster'):
+        try:
+            hehe = await message.reply_photo(photo=imdb.get('poster'), caption=cap, reply_markup=InlineKeyboardMarkup(btn))
+            await asyncio.sleep(6)
+            await hehe.delete()            
+        except (MediaEmpty, PhotoInvalidDimensions, WebpageMediaEmpty):
+            pic = imdb.get('poster')
+            poster = pic.replace('.jpg', "._V1_UX360.jpg")
+            hmm = await message.reply_photo(photo=poster, caption=cap, reply_markup=InlineKeyboardMarkup(btn))
+            await asyncio.sleep(6)            
+        except Exception as e:
+            logger.exception(e)
+            fek = await message.reply_text(text=cap, disable_web_page_preview=True, reply_markup=InlineKeyboardMarkup(btn))
+            await asyncio.sleep(6)            
+    else:
+        fuk = await message.reply_text(text=cap, disable_web_page_preview=True, reply_markup=InlineKeyboardMarkup(btn))
+        await asyncio.sleep(6)
+        await fuk.delete()
 
 @LuciferMoringstar_Robot.on_message(Worker.text & Worker.private & Worker.incoming & Worker.user(AUTH_USERS) if AUTH_USERS else Worker.text & Worker.private & Worker.incoming)
 async def pm_filters(client, message):
